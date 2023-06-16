191f4155b9b454c609eed707f7653c7156d58ed1
Rafael Vieira <rafaelvieira1333@gmail.com>
2023-06-16 18:06:30 -0300

    Update Estacionamento.js

--- Estacionamento.js
+++ Estacionamento.js
@@ -1,19 +1,22 @@
+let listAuto = [];
 
 function openApp() {
-  let modelo = document.getElementById('#inputModelo').value;
-  let placa = document.getElementById('#input-text-placa').value;  
+  let modelo = document.getElementById('inputModelo').value;
+  let placa = document.getElementById('input-text-placa').value;  
   
   if(validate(modelo, placa)) {
-    console.log(placa)
-
-}
+    listAuto.push(" Marca: "+modelo + " Placa: "+placa ); 
 }
 
+}
 
 function validate(modelo, placa){
     if (modelo.length==0 || placa.length ==0) {
       alert('Por favor preencha todos os campos')
       return false
+    }
+    else{
+      return true;
     }
   
 }
