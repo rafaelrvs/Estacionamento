d8af97a604802e2da64ea7b66b4763e896942234
rafaelrvs <rafaelvieira1333@gmail.com>
2023-09-29 18:40:24 -0300

    atualizacao

---
+++ app/src/components/Form/index.js
@@ -0,0 +1,15 @@
+import "./Form.css";
+const Form =()=>{
+  return(
+    <div className="">
+      <div className="containerForm">
+
+      </div>
+      <form id="form__container" action="">
+        <label htmlFor="Placa">Placa</label>
+        <input id="input" type="text" placeholder="Placa"/>
+      </form>
+    </div>
+  )
+}
+export default Form
