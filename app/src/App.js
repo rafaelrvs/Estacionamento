e1bae192461d76880d0bfdadc537f5121565750f
rafaelrvs <rafaelvieira1333@gmail.com>
2023-09-30 11:09:49 -0300

    atualização

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
 import Form from './components/Form';
+import TituloEstacionamento from './components/TituloEstacionamento';
 
 function App() {
   return (
     <div className="App">
+     <TituloEstacionamento/>
      <Form/>
     </div>
   );
--- app/src/components/Form/index.js
+++ app/src/components/Form/index.js
@@ -1,13 +1,21 @@
 import "./Form.css";
 const Form =()=>{
   return(
-    <div className="">
-      <div className="containerForm">
+    <div className="container__main__form">
+      <div className="containerFormFundo">
 
       </div>
       <form id="form__container" action="">
-        <label htmlFor="Placa">Placa</label>
-        <input id="input" type="text" placeholder="Placa"/>
+        <label htmlFor="modelo">Modelo</label>
+        <input id="modelo" type="text" placeholder="Digite o modelo" maxLength={30}/>
+        <div className="placaEmodelo">
+        <label htmlFor="placa">Placa</label>
+        <input id="placa" type="text" placeholder="Digite a placa" maxLength={7} required/>
+        </div>
+        <div className="containerBtn">
+           <input type="submit" value="pesquisar"/>  
+           <input type="button" value="cadastrar"/>
+        </div>
       </form>
     </div>
   )
