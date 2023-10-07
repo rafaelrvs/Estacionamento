d800d47be450d5baa4094224c8fd135886fd27df
rafaelrvs <rafaelvieira1333@gmail.com>
2023-10-06 22:25:32 -0300

    atualização

---
+++ app/src/components/FrontEnd/TituloEstacionamento/index.js
@@ -0,0 +1,10 @@
+import"./TituloEstacionamento.css";
+
+const TituloEstacionamento =()=>{
+  return(
+    <div className="containerTitle">
+        <p>Estacionamento</p>
+    </div>
+  );
+}
+export default TituloEstacionamento
